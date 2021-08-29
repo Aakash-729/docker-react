ae4eda7294f3da174be5cd40b079fe1837069018
Aakash-729 <aakashpanchal729@gmail.com>
2021-08-30 02:18:03 +0530

    Some more additions to app message

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hi there! This is my first react app!</p>
+        <p>Some more change to observer travis ci behaviour</p>
         <a
           className="App-link"
           href="https://reactjs.org"
